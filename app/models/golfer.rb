fdddcd74b5740c8a170e767020ac805e1e38ce50
James Harkins <jh.harkins@protonmail.com>
2023-01-11 14:29:17 -0700

    Feat: pass validation tests for Golfer model

--- app/models/golfer.rb
+++ app/models/golfer.rb
@@ -7,4 +7,11 @@ class Golfer < ApplicationRecord
   has_many :meals, through: :golfer_meals
   has_many :golfer_trip_courses
   has_many :trip_courses, through: :golfer_trip_courses
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates :email, uniqueness: true
+  validates_presence_of :password, require: true
+  has_secure_password
 end
