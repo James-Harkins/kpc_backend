1807f4ace3335cf472f88d06351fb6c85306aba2
James Harkins <jh.harkins@protonmail.com>
2023-01-11 14:27:25 -0700

    Test: create validation tests for Golfer model

--- spec/models/golfer_spec.rb
+++ spec/models/golfer_spec.rb
@@ -11,4 +11,28 @@ RSpec.describe Golfer, type: :model do
     it { should have_many(:golfer_trip_courses) }
     it { should have_many(:trip_courses).through(:golfer_trip_courses) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+
+    describe "email" do
+      before do
+        golfer_1 = Golfer.create!(first_name: "Tony", last_name: "Soprano", email: "wokeupthismorning@gmail.com", password: "test123", password_confirmation: "test123")
+      end
+
+      it { should allow_value("cleavermovie@gmail.com").for(:email) }
+      it { should_not allow_value("wokeupthismorning@gmail.com").for(:email) }
+    end
+
+    it { should validate_presence_of(:password) }
+    it { should have_secure_password }
+
+    it "should not have a password attribute and the password_digest attribute should be a hash" do
+      golfer = Golfer.create!(first_name: "Tony", last_name: "Soprano", email: "wokeupthismorning@gmail.com", password: "test123", password_confirmation: "test123")
+      expect(golfer).to_not have_attribute(:password)
+      expect(golfer.password_digest).to_not eq("password123")
+    end
+  end
 end
