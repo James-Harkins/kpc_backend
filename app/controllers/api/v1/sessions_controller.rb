8465558ab6200d09d28c3ad3ab5a1ebdc710f98c
James Harkins <jh.harkins@protonmail.com>
2023-01-29 17:43:37 -0700

    Feat: add session helper methods to ApplicationController and add additional actions to SessionController

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,15 +3,35 @@ class Api::V1::SessionsController < ApplicationController
   
   def create
     if authenticated?(params)
-      golfer = Golfer.find_by(email: session_params[:email])
-      if golfer&.authenticate(session_params[:password])
-        render json: GolferSerializer.new(golfer)
-      elsif golfer.nil?
+      @golfer = Golfer.find_by(email: session_params[:email])
+      if @golfer&.authenticate(session_params[:password])
+        login!
+        render json: GolferSerializer.new(@golfer)
+      elsif @golfer.nil?
         render json: {error: 'No account exists with this email.'}, status: 400
       else
         render json: {error: 'Invalid credentials.'}, status: 400
       end
     end
+  end
+
+  def is_logged_in?
+    if logged_in? && current_golfer
+      render json: {
+        logged_in: true,
+        golfer: current_golfer
+      }
+    else
+      render json: {
+        logged_in: false,
+        message: 'no such golfer'
+      }
+    end
+  end
+
+  def destroy
+    logout!
+    render json: {status: 200, logged_out: true}
   end
 
   private
